35efd63f249e1711d36222cb31d3c76bbd3b452c
Dipoo Kenchur <dkenchur@am-m1.msk.21-school.ru>
2021-03-16 20:27:34 +0300

    ready test raycasting

--- test3d.c
+++ test3d.c
@@ -7,12 +7,17 @@
 #define	height	1080//480
 #define mapWidth 24
 #define mapHeight 24
+#define spritesCount 1
+
+double zbuf[width];
+int		sprite_order[spritesCount];
+double	sprites_dist[spritesCount];
 
 int worldMap[mapWidth][mapHeight]=
-{
+{ 	
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -52,6 +57,14 @@ typedef struct	s_texture
 	int h;
 }				t_texture;
 
+typedef struct	s_sprite
+{
+	int			x;
+	int			y;
+	t_texture	img;
+}				t_sprite;
+
+t_sprite sprite[spritesCount];
 
 typedef	struct	s_vars
 {
@@ -136,6 +149,36 @@ void		draw_floor(int x, int start, int end, t_game *game, int color)
 	}
 }
 
+void		ft_sort(int *sprite_order, double *sprites_dist, int count)
+{
+	int i;
+	int j;
+	int isswap;
+	int tmp;
+
+	i = 0;
+	isswap = 0;
+	while (i < count - 1)
+	{
+		j = 0;
+		while (j < count - i - 1)
+		{
+			if (sprites_dist[j] < sprites_dist[j + 1])
+			{
+				tmp = sprites_dist[j];
+				sprites_dist[j] = sprites_dist[j + 1];
+				sprites_dist[j + 1] = tmp;
+				tmp = sprite_order[j];
+				sprite_order[j] = sprite_order[j + 1];
+				sprite_order[j + 1] = tmp;
+				isswap = 1;
+			}
+		}
+		if (!isswap)
+			break ;
+	}
+}
+
 int			raycaster(t_game *game)
 {
 	int 	x;
@@ -152,6 +195,7 @@ int			raycaster(t_game *game)
 	int		draw_start;
 	int		draw_end;
 	int		color;
+
 
 	color = 0x000000FF;
 	x = 0;
@@ -207,7 +251,7 @@ int			raycaster(t_game *game)
 				map[1] += step[1];
 				side = 1;
 			}
-			if (worldMap[map[0]][map[1]] > 0)
+			if (worldMap[map[0]][map[1]] == 1)
 				hit = 1;
 		}
 		if (!side)
@@ -258,14 +302,87 @@ int			raycaster(t_game *game)
 				color = *(unsigned int*)(game->tex2.img.addr + (tex_y * game->tex2.img.line_length + (game->tex2.img.bits_per_pixel / 8) * tex_x));
 			// printf("%d\n", tex_y * game->tex.img.line_length + (game->tex.img.bits_per_pixel / 8) * tex_x);
 			// color = *(unsigned int*)(game->tex.img.addr + (tex_x * texHeight + tex_y));
-			if (side)
-				color = (color >> 1) & 8355711;
+			// if (side)
+			// 	color = (color >> 1) & 8355711;
 			my_mlx_pixel_put(&game->img, x, y, color);
 		}
 		draw_floor(x, draw_end, height, game, 0xd4d7ea);
 		draw_sky(x, draw_start, game, 0x8aadfb);
 		// draw_vertline(x, draw_start, draw_end, game, color);
+		zbuf[x] = perp_wall_dist;
 		x++;
+	}
+
+	int i = 0;
+	sprite[0].x = 2;
+	sprite[0].y = 2;
+	// sprite[1].x = 4;
+	// sprite[1].y = 2;
+	// sprite[2].x = 2;
+	// sprite[2].y = 4;
+	while (i < spritesCount)
+	{
+		sprite_order[i] = i;
+		sprites_dist[i] = pow(game->plr.pos[0] - sprite[i].x, 2) + pow(game->plr.pos[1] - sprite[i].y, 2);
+		i++;
+	}
+	// можно сортировать сразу массив спрайтов (но не точно)
+	ft_sort(sprite_order, sprites_dist, spritesCount);
+	i = 0;
+	while (i < spritesCount)
+	{
+		double sprite_x = sprite[sprite_order[i]].x - game->plr.pos[0];
+		double sprite_y = sprite[sprite_order[i]].y - game->plr.pos[1];
+
+		double inv_det = 1.0 / (game->plr.plane[0] * game->plr.dir[1] - game->plr.plane[1] * game->plr.dir[0]);
+
+		double tr_x = inv_det * (game->plr.dir[1] * sprite_x - game->plr.dir[0] * sprite_y);
+		double tr_y = inv_det * (-game->plr.plane[1] * sprite_x + game->plr.plane[0] * sprite_y);
+
+		int sprite_scr_x = (int)((width / 2) * (1 + tr_x / tr_y));
+
+		// убрать нахер
+		#define uDiv 1
+      	#define vDiv 1
+      	#define vMove 0.0
+		// -------------
+		int v_move_scr = (int)(vMove / tr_y);
+		int sprite_h = abs((int)(height / tr_y)) / vDiv;
+		int draw_start_y = -sprite_h / 2 + height / 2 + v_move_scr;
+		if (draw_start_y < 0)
+			draw_start_y = 0;
+		int draw_end_y = sprite_h / 2 + height / 2 + v_move_scr;
+		if (draw_end_y >= height)
+			draw_end_y = height - 1;
+
+		int sprite_w = abs((int)(height / tr_y)) / uDiv;
+		int draw_start_x = -sprite_w / 2 + sprite_scr_x;
+		if (draw_start_x < 0)
+			draw_start_x = 0;
+		int draw_end_x = sprite_w / 2 + sprite_scr_x;
+		if (draw_end_x >= width)
+			draw_end_x = width - 1;
+		
+		int stripe = draw_start_x;
+		while (stripe < draw_end_x)
+		{
+			int tex_x = (int)(256 * (stripe - (-sprite_w / 2 + sprite_scr_x)) * sprite[sprite_order[i]].img.w / sprite_w) / 256;
+			if (tr_y > 0 && stripe > 0 && stripe < width && tr_y < zbuf[stripe])
+			{
+				int y_y = draw_start_y;
+				while (y_y < draw_end_y)
+				{
+					int d = (y_y - v_move_scr) * 256 - height * 128 + sprite_h * 128;
+					int tex_y = ((d * sprite[sprite_order[i]].img.h) / sprite_h) / 256;
+					color = *(unsigned int*)(sprite[sprite_order[i]].img.img.addr + (tex_y * sprite[sprite_order[i]].img.img.line_length + (sprite[sprite_order[i]].img.img.bits_per_pixel / 8) * tex_x));
+					if ((color & 0x00FFFFFF) != 0)
+						my_mlx_pixel_put(&game->img, stripe, y_y, color);
+					y_y++;
+				}
+			}
+			stripe++;
+		}
+		i++;
 	}
 	return (0);
 }
@@ -289,7 +406,7 @@ void		shift(int keycode, t_game *game)
 	double	rot_speed;
 	double 	buf;
 
-	move_speed = 0.2;
+	move_speed = 0.1;
 	rot_speed = 0.15;
 
 	// clc_img(game);
@@ -389,8 +506,8 @@ int			main()
 	game.vars.mlx = mlx_init();
 	game.vars.win = mlx_new_window(game.vars.mlx, width, height, "Game");
 
-	game.plr.pos[0] = 1.5;
-	game.plr.pos[1] = 1.5;
+	game.plr.pos[0] = 8;
+	game.plr.pos[1] = 2;
 	game.plr.dir[0] = 1;
 	game.plr.dir[1] = 0;
 	game.plr.plane[0] = 0;
@@ -406,12 +523,16 @@ int			main()
 	game.tex.img.addr = mlx_get_data_addr(game.tex.img.img, &game.tex.img.bits_per_pixel, &game.tex.img.line_length,
 		&game.tex.img.endian);
 
-	game.tex2.img.img = mlx_xpm_file_to_image(game.vars.mlx, "textures/eagle.xpm", &game.tex2.w, &game.tex2.h);
+	game.tex2.img.img = mlx_xpm_file_to_image(game.vars.mlx, "textures/mossy.xpm", &game.tex2.w, &game.tex2.h);
 	if (game.tex2.img.img == NULL)
 		exit(1);
 	game.tex2.img.addr = mlx_get_data_addr(game.tex2.img.img, &game.tex2.img.bits_per_pixel, &game.tex2.img.line_length,
 		&game.tex2.img.endian);
 
+	sprite[0].img.img.img = mlx_xpm_file_to_image(game.vars.mlx, "textures/barrel.xpm", &sprite[0].img.w, &sprite[0].img.h);
+	if (sprite[0].img.img.img == NULL)
+		exit(1);
+	sprite[0].img.img.addr = mlx_get_data_addr(sprite[0].img.img.img, &sprite[0].img.img.bits_per_pixel, &sprite[0].img.img.line_length, &sprite[0].img.img.endian);
 	// raycaster(&game);
 
 	mlx_hook(game.vars.win, 17, 0, &win_close, &game);
